cfe555000c4d790373a068927b8d992c8e0a200e
TheQuasarS50014p81 <20722202+TheQuasarS50014p81@users.noreply.github.com>
2021-04-15 15:41:42 +0900

    graphql variables and mutation

--- src/graphql.js
+++ src/graphql.js
@@ -1,10 +1,36 @@
 import gql from 'graphql-tag'
 
-export const ME = gql`
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!){
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY){
+      repositoryCount
+      pageInfo{
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges{
+        cursor
+        node{
+          ... on Repository{
+            id
+            name
+            url
+            stargazers{
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  }`
+
+/*export const ME = gql`
   query me{ 
     user(login: "TheQuasarS50014p81") {
       name
       avatarUrl
     }
   }
-`
+`*/
